34b9e64e38e47472328f123916ff825e573b97b9
luisemiliotisocco <luisemiliotisocco@gmail.com>
2021-08-20 09:53:40 -0300

    actualizacion ejercicio2

--- Ejercicio-2.R
+++ Ejercicio-2.R
@@ -14,43 +14,31 @@ url <- "https://www.cronista.com/MercadosOnline/dolar.html" #cargo la URL
 #importo la tabla por columna
 
 moneda <- read_html(url) %>%
-  html_nodes(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "name", " " )) and contains(concat( " ", @class, " " ), concat( " ", "col", " " ))]
-') %>%
+  html_nodes(xpath = '//*[(@id = "market-scrll-1")]//*[contains(concat( " ", @class, " " ), concat( " ", "name", " " ))]') %>%
   html_text2()
 
-variacion <- read_html(url) %>% 
-  html_nodes(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "percentage", " " )) and contains(concat( " ", @class, " " ), concat( " ", "col", " " ))]
-') %>%
+  variacion <- read_html(url) %>% 
+  html_nodes(xpath = '//*[(@id = "market-scrll-1")]//*[contains(concat( " ", @class, " " ), concat( " ", "percentage", " " ))]') %>%
   html_text() %>% 
   str_replace_all("[^[:alnum:]]", "") %>% #elimino caracteres que no sean alfanuméticos 
   as.numeric()
 
-compra <- read_html(url) %>% 
-  html_nodes(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "buy", " " )) and contains(concat( " ", @class, " " ), concat( " ", "col", " " ))]') %>%
-  html_text() %>% 
-  substring(8) %>% #elimino el string "Compra$"
-  str_replace_all("[^[:alnum:]]", "") %>% #elimino caracteres que no sean alfanuméticos 
-  as.numeric()
-
-venta <- read_html(url) %>% 
-  html_nodes(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "sell", " " )) and contains(concat( " ", @class, " " ), concat( " ", "col", " " ))]') %>%
-  html_text() %>% 
-  str_replace_all("[^[:alnum:]]", "") %>% #elimino caracteres que no sean alfanuméticos 
+valor <- read_html(url) %>% 
+  html_nodes(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "value", " " ))]') %>%
+  html_text() %>%
+  str_replace_all ("[^[:alnum:]]", "") %>% #elimino caracteres que no sean alfanuméticos 
   as.numeric()
 
 #uno las columnas en una tabla
 tabla <- tibble(
-  moneda = moneda, 
-  variacion = variacion, 
-  compra = compra,
-  venta= venta
-)
+  moneda = moneda,
+  variacion=variacion, 
+  valor= valor)
 
 cotizacion <- tabla %>% 
   slice(2:nrow(tabla)) %>% #elimino primera fila que trae el nombre de las columnas
   mutate(variacion=variacion/100) %>%  #agrego los decimales que eliminé anteriormente
-  mutate(compra=compra/100) %>% 
-  mutate(venta=venta/100)
+  mutate(valor=valor/100)
 
 str (cotizacion) #compruebo la estructura de los datos. Todos los valores (experto el nombre de la moneda) son numéricos
 
